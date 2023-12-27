e4b28a3ffdb25f587326774da5787ec5559b7bad
thetrickuser <coolady1807@outlook.com>
2023-12-27 19:33:09 +0530

    added rating and price filters

--- frontend/src/SearchResults.jsx
+++ frontend/src/SearchResults.jsx
@@ -1,16 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import Product from "./Product";
 import PropTypes from "prop-types";
 
-const SearchResults = ({ results }) => {
-  const [filteredProducts, setFilteredProducts] = useState(results);
-  const [filters, setFilters] = useState({
+const initialState = {
+  filteredProducts: [],
+  filters: {
     price: { min: "", max: "" },
     category: ["All"],
     rating: "",
-  });
+  },
+  minPrice: "",
+  maxPrice: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_FILTERED_PRODUCTS":
+      return { ...state, filteredProducts: action.payload };
+    case "SET_FILTERS":
+      return { ...state, filters: action.payload };
+    case "SET_MIN_PRICE":
+      return { ...state, minPrice: action.payload };
+    case "SET_MAX_PRICE":
+      return { ...state, maxPrice: action.payload };
+    default:
+      return state;
+  }
+};
+
+const SearchResults = ({ results }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { filteredProducts, filters, minPrice, maxPrice } = state;
 
   useEffect(() => {
+    const filtered = results.filter(
+      (item) => item.price >= minPrice && item.price <= maxPrice
+    );
+    dispatch({ type: "SET_FILTERED_PRODUCTS", payload: filtered });
+  }, [minPrice, maxPrice, results]);
+
+  useEffect(() => {
+    console.log(results)
     let categories = new Set();
     results.forEach((item) => categories.add(item.category));
     categories = Array.from(categories);
@@ -25,21 +55,41 @@ const SearchResults = ({ results }) => {
       { minPrice: Infinity, maxPrice: -Infinity }
     );
 
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      price: { min: minPrice, max: maxPrice },
-      category: ["All", ...categories],
-    }));
+    dispatch({
+      type: "SET_FILTERS",
+      payload: {
+        price: { min: minPrice, max: maxPrice },
+        category: ["All", ...categories],
+      },
+    });
+    dispatch({ type: "SET_MIN_PRICE", payload: filters.price.min });
+    dispatch({ type: "SET_MAX_PRICE", payload: filters.price.max });
   }, [results]);
 
   const handleCategoryChange = (filterCategory) => {
     if (filterCategory === "All") {
-      setFilteredProducts(results);
+      dispatch({ type: "SET_FILTERED_PRODUCTS", payload: results });
+      dispatch({ type: "SET_MIN_PRICE", payload: filters.price.min });
+      dispatch({ type: "SET_MAX_PRICE", payload: filters.price.max });
     } else {
-      setFilteredProducts(
-        results.filter((item) => item.category === filterCategory)
+      const filtered = results.filter(
+        (item) => item.category === filterCategory
       );
+      dispatch({ type: "SET_FILTERED_PRODUCTS", payload: filtered });
     }
+  };
+
+  const handleMaxPriceChange = (price) => {
+    dispatch({ type: "SET_MAX_PRICE", payload: price });
+  };
+
+  const handleMinPriceChange = (price) => {
+    dispatch({ type: "SET_MIN_PRICE", payload: price });
+  };
+
+  const handleRatingChange = (rating) => {
+    const filtered = results.filter((item) => Number(item.rating.rate) >= rating);
+    dispatch({ type: "SET_FILTERED_PRODUCTS", payload: filtered });
   };
 
   return (
@@ -48,7 +98,7 @@ const SearchResults = ({ results }) => {
         <div className="filter">
           <p>Category</p>
           {filters.category.map((item) => (
-            <div
+            <button
               key={item}
               onClick={() => {
                 handleCategoryChange(item);
@@ -56,23 +106,37 @@ const SearchResults = ({ results }) => {
               value={item}
             >
               {item}
-            </div>
+            </button>
           ))}
         </div>
         <div className="filter">
           <p>Price</p>
           <span>Min: </span>
-          <input name="min_price" type="number"></input>
+          <input
+            name="min_price"
+            type="number"
+            value={minPrice}
+            onChange={(event) => {
+              handleMinPriceChange(event.target.value);
+            }}
+          ></input>
           <span>Max: </span>
-          <input name="max_price" type="number"></input>
+          <input
+            name="max_price"
+            type="number"
+            value={maxPrice}
+            onChange={(event) => {
+              handleMaxPriceChange(event.target.value);
+            }}
+          ></input>
         </div>
         <div className="filter">
           <p>Rating</p>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+          {[1, 2, 3, 4, 5].map((item) => (
+            <button key={item} onClick={() => handleRatingChange(item)}>
+              {item}
+            </button>
+          ))}
         </div>
       </div>
       <div className="results">
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {Provider} from 'react-redux';
-import App from './App.jsx'
-import './index.css'
-import store from './store/index.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import "./index.css";
+import store from "./store/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
